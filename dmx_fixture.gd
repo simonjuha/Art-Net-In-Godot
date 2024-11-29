11c7efb0a2282e82fe3f3f749ac23d832f1c9c4d
SImon <33484974+simonjuha@users.noreply.github.com>
2024-11-29 13:04:37 +0100

    use enum instead of string
    changed dmx channel names to use enum and processing whole dmx set at once

--- dmx_fixture.gd
+++ dmx_fixture.gd
@@ -7,27 +7,28 @@ var local_dmx_data: Array 					# local data kept in lamp
 var dmx_channel_names: Array[String] = []	# names of the channels (dimmer, red, etc.)
 var total_channels: int = 0					# total number of channels - increases with each added channel
 
-func process_dmx(dmx_data_array: Array) -> void:
-	# se if local data is different from the new data
-	if local_dmx_data != dmx_data_array.slice(dmx_channel_offset, total_channels):
-		local_dmx_data = dmx_data_array.slice(dmx_channel_offset, total_channels)
 
-		for i in range(min(dmx_data_array.size(), dmx_channel_names.size())):
-			var dmx_data = dmx_data_array[i]
-			process_light(dmx_channel_names[i], dmx_data)
-	pass
 
-func add_channel(channel_name: String) -> void:
-	dmx_channel_names.append(channel_name)
-	total_channels += 1
-	add_to_group("dmx_fixtures") # added here to avoid overring the _ready function, might change
+func set_fixture_channels(offset: int, channel_count: int) -> void:
+	dmx_channel_offset = offset
+	total_channels = channel_count + 1
+	add_to_group("dmx_fixtures")
 	pass
 
 func add_to_dmx_universe(artnet: ArtNet):
-	artnet.connect("dmx_updated", Callable(self, "process_dmx"))
+	artnet.connect("dmx_updated", Callable(self, "_check_dmx"))
 	pass
 
+func _check_dmx(dmx_data_array: Array) -> void:
+# se if local data is different from the new data
+	var temp_local_dmx_data = dmx_data_array.slice(dmx_channel_offset, total_channels)
+	if temp_local_dmx_data != local_dmx_data:
+		local_dmx_data = temp_local_dmx_data
+		# process the data
+		process_dmx(local_dmx_data)
+
+
 # This function should be overridden in the derived class, processing dmx property names and values
-func process_light(channel_name: String, channel_value: int):
-	push_error("The 'required_method' must be overridden in the derived class!") # Replace with function body.
+func process_dmx(dmx: Array):
+	push_error("The 'process_dmx' must be overridden in the derived class!") # Replace with function body.
 pass
